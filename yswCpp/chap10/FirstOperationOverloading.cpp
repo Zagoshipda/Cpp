2ee4e655a3e1f8d9df60ddac7701c286c8fbb627
Zagoshipda <john713@hanmail.net>
2019-05-08 22:47:07 +0900

    operator + overloading

---
+++ yswCpp/chap10/FirstOperationOverloading.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+#define endl '\n'
+
+class Point{
+    private:
+        int xpos, ypos;
+    public:
+        Point(int x=0, int y=0): xpos(x), ypos(y){
+
+        }
+        void showPosition(){
+            cout << '[' << xpos << ", " << ypos << ']' << endl;
+        }
+        Point operator+(const Point &ref){
+            Point pos(xpos+ref.xpos, ypos+ref.ypos);
+            return pos;
+        }
+};
+
+int main(void){
+    Point pos1(1,2);
+    Point pos2(3,4);
+    Point pos3 = pos1.operator+(pos2);
+    pos1.showPosition();
+    pos2.showPosition();
+    pos3.showPosition();
+    
+    return 0;
+}
