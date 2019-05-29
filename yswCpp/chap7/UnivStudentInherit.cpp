89e686a6d43677fb1829c85940432d7247bc0ce1
Zagoshipda <john713@hanmail.net>
2019-05-30 03:14:35 +0900

    Inheritance ex. Person - UnivStudent with information hiding

--- yswCpp/chap7/UnivStudentInherit.cpp
+++ yswCpp/chap7/UnivStudentInherit.cpp
@@ -10,6 +10,7 @@ class Person{
     public:
         Person(int myage, const char* myname): age(myage){
             strcpy(name, myname);
+            // name = myname;  //expression must be a modifiable lvalue
         }
         void PrintName() const{
             cout << "name: " << name << endl;
@@ -23,7 +24,7 @@ class UnivStudent : public Person{
     private:
         char major[50];
     public:
-        //constructor, 유도 클래스에서의 초기화 작업은 기초 클래스인 Person class의 멤버까지 초기화의 범위에 포함하며, 이 과정은 기초 클래스의 생성자를 (member initializer를 이용하여 호출)하는 것으로 보다 간단하게 수행할 수 있다.
+        //constructor, 유도 클래스에서의 초기화 작업은 기초 클래스인 Person class의 멤버까지 초기화의 범위에 포함하며, 이 과정은 기초 클래스의 생성자를 <member initializer를 이용하여 호출>하는 것으로 보다 간단하게 수행할 수 있다.
         UnivStudent(const char* myname, int myage, const char* mymajor): Person(myage, myname){
             strcpy(major, mymajor);
         }
